e7afc26ab2feafdc3a89686413cfdd15c3fbd201
KPq66dw8L <kezhkia@protonmail.com>
2021-04-08 13:15:13 +0200

    JS:20%

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-// When the user clicks the button, open the modal 
-let btn = document.getElementById(open-modal1);
-let a = document.getElementsByClassName(modal-close);
+let clo = document.getElementsByClassName("modal-close");
+let mod = document.querySelectorAll("a[href='#open-modal1']");
 
-btn.onclick = function() {
-    modal.style.display = "block";
-    document.body.style.overflow = "hidden"; // ADD THIS LINE
-    document.body.style.height = "100%"; // ADD THIS LINE
+
+function f1() {
+    document.body.style.overflow = "hidden"; 
+    document.body.style.height = "80vh"; 
 }
 
-// When the user clicks on <a> (x), close the modal
-a.onclick = function() {
-  modal.style.display = "hidden";
-    document.body.style.overflow = "auto"; // ADD THIS LINE
-    document.body.style.height = "auto"; // ADD THIS LINE
+function f2() {
+    document.body.style.overflow = "auto"; 
+    document.body.style.height = "auto"; 
 }
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     a.style.display = "hidden";
-    document.body.style.overflow = "auto"; // ADD THIS LINE
-    document.body.style.height = "auto";  // ADD THIS LINE
+    document.body.style.overflow = "auto"; 
+    document.body.style.height = "auto";  
   }
-}
+} 
