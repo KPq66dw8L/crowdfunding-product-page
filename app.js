c8036267abac0de6fb79a281b6f0fc47253eac31
KPq66dw8L <kezhkia@protonmail.com>
2021-04-08 14:24:44 +0200

    JS:30% left:bar,radio btn & div link,pledge appearing when clicking on div, pre-selected pledge + adjusting CSS to fit first modal completely

--- app.js
+++ app.js
@@ -5,19 +5,24 @@ let mod = document.querySelectorAll("a[href='#open-modal1']");
 function f1() {
     document.body.style.overflow = "hidden"; 
     document.body.style.height = "80vh"; 
+    document.body.style.boxShadow = "inset -30px -400px 300px 200px #f5f4f4";
+    
 }
 
 function f2() {
     document.body.style.overflow = "auto"; 
     document.body.style.height = "auto"; 
+    document.body.style.boxShadow = "none";
+    
 }
 
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+
+/* window.onclick = function(event) {
   if (event.target == modal) {
     a.style.display = "hidden";
     document.body.style.overflow = "auto"; 
     document.body.style.height = "auto";  
   }
-} 
+}  */
