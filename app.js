fd512f9ed9dc5dd98f05822691a8ebddf2f69ffd
KPq66dw8L <kezhkia@protonmail.com>
2021-04-08 11:02:06 +0200

    html:100% CSS:90% JS:1%

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-document.getElementsByClassName("");
+// When the user clicks the button, open the modal 
+let btn = document.getElementById(open-modal1);
+let a = document.getElementsByClassName(modal-close);
+
+btn.onclick = function() {
+    modal.style.display = "block";
+    document.body.style.overflow = "hidden"; // ADD THIS LINE
+    document.body.style.height = "100%"; // ADD THIS LINE
+}
+
+// When the user clicks on <a> (x), close the modal
+a.onclick = function() {
+  modal.style.display = "hidden";
+    document.body.style.overflow = "auto"; // ADD THIS LINE
+    document.body.style.height = "auto"; // ADD THIS LINE
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    a.style.display = "hidden";
+    document.body.style.overflow = "auto"; // ADD THIS LINE
+    document.body.style.height = "auto";  // ADD THIS LINE
+  }
+}
